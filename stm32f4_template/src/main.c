4f95784a583c50d1b6098a2f4ba82a30f29bcc94
Zachary <zmoshansky@gmail.com>
2014-10-30 19:33:38 -0700

    Properly added stm32F4_template to project

---
+++ stm32f4_template/src/main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+// Private function prototypes
+void init();
+
+int main(void) {
+	init();
+
+	return 0;
+}
+
+void init() {
+	
+}
+
+/*
+ * Dummy function to avoid compiler error
+ */
+void _init() {
+
+}
